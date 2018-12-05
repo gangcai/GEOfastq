71a94a73d41eb2b9d40700c3a8231e5f66c6ab6b
Gangcai Xie <gcxiester@gmail.com>
2018-12-05 14:44:37 +0800

    correct path

--- run3a_sra2fq.py
+++ run3a_sra2fq.py
@@ -1,10 +1,10 @@
 import os
-for line in open("../SRA/GSM_info.2.tsv"):
+for line in open("GSM_info.tsv"):
 	line=line.rstrip()
-	sr=line.split("\t")[-2]
-	sra_file="../SRA/"+sr+"/"+sr+".sra"
+	sr=line.split("\t")[-1]
+	sra_file=sr+"/"+sr+".sra"
 	fq=sr+".fq.gz"
 	if os.path.isfile(fq):
 		print(sr+" skipped")
 		continue
-	os.system("fasterq-dump {0} -S -b 1000MB -m 20000MB -e 10 -o {1}".format(sra_file,sr))
+	os.system("fasterq-dump {0} -S -b 1000MB -m 20000MB -e 10 -o {1}.fq".format(sra_file,sr))
--- run3b_gzip.py
+++ run3b_gzip.py
@@ -1,6 +1,5 @@
 import glob,os
-for filename in glob.glob("SRX*"):
+for filename in glob.glob("SRX*.fq"):
 	sample=filename
-	os.system("mv {0} {0}.fq".format(sample))
-	os.system("nohup gzip {0}.fq > {0}.log &".format(sample))
+	os.system("nohup gzip {0} > {0}.log &".format(sample))
 
